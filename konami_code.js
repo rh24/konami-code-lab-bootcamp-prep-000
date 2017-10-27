1f21e0f66d136713eafee4fcf5f16564d3b6e09d
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-27 00:21:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-// let index = 0;
-//
-// function init(e) {
-//   // Write your JavaScript code inside the init() function
-//   const key = parseInt(e.detail || e.which);
-//
-//   if (key === code[index]) {
-//     index++;
-//
-//     if (index === code.length) {
-//       alert("You did it!");
-//
-//       index = 0;
-//     }
-//   } else {
-//     index = 0;
-//   }
-// }
-
 document.addEventListener('keydown', (e) => {
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   let index = 0;
 
   function init(e) {
