7e67f68107db81ca7d3401712c681bc12e785f13
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-27 00:22:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-document.addEventListener('keydown', (e) => {
+document.body.addEventListener('keydown', (e) => {
   const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   let index = 0;
 
