746641f37e4b4c606598f0bd89dd661d7afdf313
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-27 00:16:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,4 +5,8 @@ let index = 0;
 function init(e) {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    
+  }
 }
