421e69f4edd0aeac731b3e78fd4417f951024832
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-27 00:20:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,17 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 // let index = 0;
-// 
+//
 // function init(e) {
 //   // Write your JavaScript code inside the init() function
 //   const key = parseInt(e.detail || e.which);
-// 
+//
 //   if (key === code[index]) {
 //     index++;
-// 
+//
 //     if (index === code.length) {
 //       alert("You did it!");
-// 
+//
 //       index = 0;
 //     }
 //   } else {
@@ -38,4 +38,4 @@ document.addEventListener('keydown', (e) => {
       index = 0;
     }
   }
-}
+});
