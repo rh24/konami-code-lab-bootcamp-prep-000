8a7dd116f958adc1c73f4a098537a962bc6b1069
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-27 00:24:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
-document.body.addEventListener('keydown', (e) => {
+document.body.addEventListener('keydown', function init(e) => {
 
   function init(e) {
     // Write your JavaScript code inside the init() function
